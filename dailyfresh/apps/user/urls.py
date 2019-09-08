6649aafd6686eb9f994889aea83fb0864bed46b3
ndsgbaqq@126.com <ndsgbaqq@126.com>
2019-09-08 22:19:38 +0800

    create register

--- dailyfresh/apps/user/urls.py
+++ dailyfresh/apps/user/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
+from user.views import RegisterView
 
 urlpatterns = [
-
+    path('register', RegisterView.as_view(), name='register')  # 注册
 ]
--- dailyfresh/apps/user/views.py
+++ dailyfresh/apps/user/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
+from user.models import User, Address
 
 # Create your views here.
+
+
+class RegisterView(View):
+
+    def get(self, request):
+        return render(request, 'register.html')
+
+    def post(self, request):
+        username = request.POST.get('user_name')
+        password = request.POST.get('pwd')
+        email = request.POST.get('email')
+        allow = request.POST.get('allow')
+
+        if not all([username, password, email]):
+            return render(request, 'register.html', {'error': '数据不完整！'})
+
+        if allow != 'on':
+            return render(request, 'register.html', {'error': '请勾选同意！'})
+
+        # 检查用户名是否存在
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            user = None
+
+        if user:
+            render(request, 'register.html', {'error': '用户名已存在'})
+
+        user = User.objects.create(
+            username=username, password=password, email=email, is_active=0)
+        user.save()
+        return redirect('/')
