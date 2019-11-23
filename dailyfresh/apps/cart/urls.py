7bf287463163ffc8094f5cdbde7137a42433ad21
ndsgbaqq@126.com <ndsgbaqq@126.com>
2019-11-23 15:54:44 +0800

    update cart

--- dailyfresh/apps/cart/urls.py
+++ dailyfresh/apps/cart/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
+from cart.views import CartAddView, CartInfoView
 
 urlpatterns = [
-
+    path('add', CartAddView.as_view(), name='add'),
+    path('',CartInfoView.as_view(),name='show')
 ]
--- dailyfresh/apps/cart/views.py
+++ dailyfresh/apps/cart/views.py
@@ -1,3 +1,68 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.views.generic import View
+from django.http import JsonResponse
+from goods.models import GoodsSKU
+from django_redis import get_redis_connection
+from utils.mixin import LoginRequiredMixin
 # Create your views here.
+
+
+class CartAddView(View):
+    def post(self, request):
+        sku_id = request.POST.get('sku_id')
+        count = request.POST.get('count')
+        user = request.user
+        if not user.is_authenticated:
+            return JsonResponse({'res': 4, 'error': '请登录'})
+
+        if not all([sku_id, count]):
+            return JsonResponse({'res': 0, 'error': '数据不完整'})
+
+        try:
+            count = int(count)
+        except Exception as e:
+            return JsonResponse({'res': 1, 'error': '商品数量出错'})
+
+        try:
+            sku = GoodsSKU.objects.get(id=sku_id)
+        except GoodsSKU.DoesNotExist:
+            return JsonResponse({'res': 2, 'error': '商品不存在'})
+
+        conn = get_redis_connection('default')
+        cart_key = 'cart_%d' % user.id
+        cart_count = conn.hget(cart_key, sku_id)
+
+        if cart_count:
+            count += int(cart_count)
+        if count > sku.stock:
+            return JsonResponse({'res': 3, 'error': '商品库存不足'})
+        conn.hset(cart_key, sku_id, count)
+
+        total_count = conn.hlen(cart_key)
+        return JsonResponse({'res': 5, 'message': '商品数量出错', 'total_count': total_count})
+
+
+class CartInfoView(LoginRequiredMixin, View):
+    def get(self, request):
+        # 获取用户购物车信息
+        user = request.user
+        conn = get_redis_connection('default')
+        cart_key = 'cart_%d' % user.id
+        cart_dict = conn.hgetall(cart_key)
+        skus = []
+        total_count = 0
+        total_price = 0
+        # 遍历获取的商品信息
+        for sku_id, count in cart_dict.items():
+            sku = GoodsSKU.objects.get(id=sku_id)
+            amount = sku.price*int(count)
+            sku.count = int(count)
+            sku.amount = amount
+            total_count += int(count)
+            total_price += amount
+            skus.append(sku)
+
+        # 组织上下文
+        context = {'total_count': total_count,
+                   'total_price': total_price, 'skus': skus}
+        return render(request, 'cart.html',context)
